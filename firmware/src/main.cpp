909ce4b8e074530b5ac426f038fa3b5617e0f641
tristenangell <tristen@neon.localdomain>
2020-10-15 20:53:00 -0400

    added program outline

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,1 +1,17 @@
 #include <mbed.h>
+
+//definitions and inits
+
+//read vss
+
+//read position over pins 16/17
+
+//store start position when button pushed
+
+//when vehicle starts moving from vss and position start timer
+//determine direcition of motion here
+
+//when gps coords approach start coords within x () meters, determine distance to 
+//start coords and calculate remaining time to coords using speed and acceleration
+
+//send calculated lap time over can
